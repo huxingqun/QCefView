9eeea8e4dd8954c5ddb8038444ebd6ff03594ef4
huxingqun <huxingqun@axzt.net>
2024-04-11 22:55:50 +0800

    changes: try use cef version 109.1.18

--- cmake/CefViewCoreConfig.cmake
+++ cmake/CefViewCoreConfig.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 # fetch CefViewCore project
 FetchContent_Declare(
     CefViewCore
-    GIT_REPOSITORY https://github.com/CefView/CefViewCore.git
+    GIT_REPOSITORY https://github.com/huxingqun/CefViewCore.git
     GIT_TAG c79fb7f5fe1e697dca3a501570daf30e794ceaeb
 )
 
@@ -20,7 +20,7 @@ FetchContent_Declare(
 # "107.1.9+g1f0a21a+chromium-107.0.5304.110"  # BAD   # debugbreak - debug mode
 # "110.0.26+g732747f+chromium-110.0.5481.97"  # BAD   # TO-BE-FIXED
 # "113.3.1+g525fa10+chromium-113.0.5672.128"  # So far so good
-set(CEF_SDK_VERSION "113.3.1+g525fa10+chromium-113.0.5672.128")
+set(CEF_SDK_VERSION "109.1.18+gf1c41e4+chromium-109.0.5414.120")
 
 # populate CefViewCore project
 FetchContent_MakeAvailable(CefViewCore)
